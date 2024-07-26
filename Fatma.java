6b4df397388b2bef376024bbf16e8320d881d96a
fatmaamr22 <fatmaamro34@gmail.com>
2024-07-26 15:18:03 +0300

    class edit

--- Fatma.java
+++ Fatma.java
@@ -1,5 +1,6 @@
 public class Fatma{
     public static void main(String args[]){
-        System.out.println("Hello Fatom.")
+        System.out.println("Hello Fatom.");
+        System.out.println("JAVA");
     }
 }
