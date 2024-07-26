00f2b25ad8d01bf01ffc7992b7b8859f7f8d9145
waleedelhamey <waleedkamal1999@gmail.com>
2024-07-26 15:40:06 +0300

    Hello fatom to hello ay 7aga

--- Fatma.java
+++ Fatma.java
@@ -1,6 +1,6 @@
 public class Fatma{
     public static void main(String args[]){
-        System.out.println("Hello Fatom.");
+        System.out.println("Hello ay 7aga");
         System.out.println("JAVA");
     }
 }
