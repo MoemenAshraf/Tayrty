60f8797f28e366be1e4da19c8c46f8520ff0c0a7
MoYaserr <mohamedd.yasserr15@gmail.com>
2024-07-26 15:35:40 +0300

    Yasser's File

---
+++ Yasser.java
@@ -0,0 +1,20 @@
+public class Test {
+
+    public static void main(String[] args)
+	{
+        System.out.println("Hey Waleed!");
+		Yasser Y=new Yasser();
+	
+    }
+	
+	
+}
+
+
+  class Yasser
+{
+		public Yasser()
+		{System.out.println("Hello Dears");}
+	
+		
+}
