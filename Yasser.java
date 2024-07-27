5111685d2ed1626f631b8afcbc58c198d1441f87
fatmaamr22 <fatmaamro34@gmail.com>
2024-07-27 14:32:56 +0300

    fatma edit

--- Yasser.java
+++ Yasser.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args)
 	{
-        System.out.println("Hey Waleed!");
+        System.out.println("Hey Fatma!");
 		Yasser Y=new Yasser();
 	
     }
