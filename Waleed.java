9b278bbd600c7d868e880d5b7fb6a98d549f6301
waleedelhamey <waleedkamal1999@gmail.com>
2024-07-27 14:47:41 +0300

    Updating class Waleed in branch2 after Fatma

--- Waleed.java
+++ Waleed.java
@@ -2,7 +2,7 @@ class Waleed{
 
 	public static void main(String[] args){
 		System.out.println("New Class Waleed added to branch2");
-		System.out.println("Fatma added to branch2");
+		System.out.println("Fatma and Waleed added to branch2");
 	} 
 
 
